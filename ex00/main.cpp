baef6735334b16f5efc1dfa00b8a0cb86eb86cfe
Kaouthar Kouaz <kkouaz@e2r4p11.1337.ma>
2023-12-22 01:58:59 +0100

    ok

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,40 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkouaz <kkouaz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/11 22:19:49 by kkouaz            #+#    #+#             */
-/*   Updated: 2023/12/21 23:46:04 by kkouaz           ###   ########.fr       */
+/*   Created: 2023/12/21 23:46:08 by kkouaz            #+#    #+#             */
+/*   Updated: 2023/12/21 23:49:07 by kkouaz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EASYFIND_HPP
-#define EASYFIND_HPP
+#include"easyfind.hpp"
 
-
-#include<iostream>
- 
-template<typename T>
-
-int easyfind(T& myContainer, int a)
+int main() 
 {
 
-    typename T :: iterator iter;
-    iter = std :: find(myContainer.begin(), myContainer.end(), a);
-    if(iter == myContainer.end)
-        throw(std :: runtime_error("value not found"));
-    return(*iter);
 }
-
-
-
-
-
-
-
-
-
-
-#endif
